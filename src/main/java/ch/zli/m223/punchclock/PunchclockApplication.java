901bd965d5e5ad09c0d64ea283ef79aedcea1d31
Wolf Chris <chris.wolf@bluewin.ch>
2019-10-31 15:56:52 +0100

    Added some more security stuff

--- src/main/java/ch/zli/m223/punchclock/PunchclockApplication.java
+++ src/main/java/ch/zli/m223/punchclock/PunchclockApplication.java
@@ -2,11 +2,13 @@ package ch.zli.m223.punchclock;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class PunchclockApplication {
 
+	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
