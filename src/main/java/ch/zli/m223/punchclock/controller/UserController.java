8143fb4c094a57986b44fe0e11324e3110128415
Wolf Chris <chris.wolf@bluewin.ch>
2019-10-31 15:14:43 +0100

    Added methods to user controller

--- src/main/java/ch/zli/m223/punchclock/controller/UserController.java
+++ src/main/java/ch/zli/m223/punchclock/controller/UserController.java
@@ -1,5 +1,24 @@
 package ch.zli.m223.punchclock.controller;
 
+import ch.zli.m223.punchclock.domain.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import ch.zli.m223.punchclock.repository.UserRepository;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 public class UserController {
-    
+
+    private UserRepository userRepository;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public UserController(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userRepository = userRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    @PostMapping("/sign-up")
+    public void signUp(@RequestBody User user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+    }
 }
