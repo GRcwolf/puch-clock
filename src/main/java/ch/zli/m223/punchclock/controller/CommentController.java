43976539be79438164dde0f1fa317b414827f264
Wolf Chris <chris.wolf@bluewin.ch>
2019-11-01 16:26:51 +0100

    WIP: implement comments

--- src/main/java/ch/zli/m223/punchclock/controller/CommentController.java
+++ src/main/java/ch/zli/m223/punchclock/controller/CommentController.java
@@ -1,9 +1,15 @@
 package ch.zli.m223.punchclock.controller;
 
+import ch.zli.m223.punchclock.service.CommentService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/controller")
 public class CommentController {
+    private CommentService commentService;
+
+    public CommentController(CommentService commentService) {
+        this.commentService = commentService;
+    }
 }
--- src/main/java/ch/zli/m223/punchclock/service/CommentService.java
+++ src/main/java/ch/zli/m223/punchclock/service/CommentService.java
@@ -1,4 +1,20 @@
 package ch.zli.m223.punchclock.service;
 
+import ch.zli.m223.punchclock.domain.Comment;
+import ch.zli.m223.punchclock.repository.CommentRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
 public class CommentService {
+    private CommentRepository commentRepository;
+
+    public CommentService(CommentRepository commentRepository) {
+        this.commentRepository = commentRepository;
+    }
+
+    public Comment createComment(long entryId, Comment comment) {
+
+    }
 }
