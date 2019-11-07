a2ebad7529a690dfdfe5e139fdd257cd90f916e9
Wolf Chris <chris.wolf@bluewin.ch>
2019-11-07 15:43:47 +0100

    Added sample data

--- src/main/java/ch/zli/m223/punchclock/PunchclockApplication.java
+++ src/main/java/ch/zli/m223/punchclock/PunchclockApplication.java
@@ -1,9 +1,17 @@
 package ch.zli.m223.punchclock;
 
+import ch.zli.m223.punchclock.domain.Entry;
+import ch.zli.m223.punchclock.domain.User;
+import ch.zli.m223.punchclock.repository.EntryRepository;
+import ch.zli.m223.punchclock.repository.UserRepository;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.time.LocalDateTime;
+import java.time.Month;
 
 @SpringBootApplication
 public class PunchclockApplication {
@@ -15,5 +23,37 @@ public class PunchclockApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PunchclockApplication.class, args);
+	}
+
+	/**
+	 * Create some sample data.
+	 *
+	 * @param entryRepository
+	 * 	The entry repository.
+	 * @param userRepository
+	 * 	The user repository.
+	 * @return
+	 * 	ApplicationRunner.
+	 */
+	@Bean
+	public ApplicationRunner sampleData(EntryRepository entryRepository, UserRepository userRepository) {
+		return args ->  {
+			BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+
+			// Create sample user.
+			User user = new User();
+			user.setPassword(bCryptPasswordEncoder.encode("toor"));
+			user.setUsername("root");
+			userRepository.save(user);
+
+			// Create sample entry.
+			Entry entry = new Entry();
+			LocalDateTime checkIn = LocalDateTime.of(2019, Month.NOVEMBER, 29, 9, 30, 40);
+			LocalDateTime checkOut = LocalDateTime.of(2019, Month.NOVEMBER, 29, 19, 30, 40);
+			entry.setCheckIn(checkIn);
+			entry.setCheckOut(checkOut);
+			entry.setUserId(1);
+			entryRepository.save(entry);
+		};
 	}
 }
